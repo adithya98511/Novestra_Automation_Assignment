f8a7f7392ecc4c3e7e5e889f241f4d12d12342b9
Supathi <r.supathi@gmail.com>
2025-07-11 10:42:44 +0530

    Initial Selenium Java project setup

---
+++ src/test/java/GoogleSearchTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+    /**
+     * for checking if Selenium TestNG testing works
+     * */
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void openGoogle() {
+        driver.get("https://www.google.com");
+        System.out.println("Title: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void teardown(){
+        driver.quit();
+    }
+}
